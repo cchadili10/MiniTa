4e14030c2b2cfc43d2a3e3d3daae7bf61985a408
Hamza chadili <chadilisafi@gmail.com>
2024-03-01 23:51:02 +0100

    sfg

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: hchadili <hchadili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:29:53 by hchadili          #+#    #+#             */
-/*   Updated: 2024/03/01 17:44:09 by hchadili         ###   ########.fr       */
+/*   Updated: 2024/03/01 23:48:15 by hchadili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,98 +16,41 @@
 #include <string.h>
 #include <unistd.h>
 
-
-void rev_array(char *array, int i)
+void decmal_to_bin(int al, int id_pd)
 {
-	int	x;
-	int y;
-	char swp;
+	int i;
+	int bit;
 
-	x = 0;
-	while (i > x)
+	i = 7;
+	while (i>=0)
 	{
-		swp = array[x];
-		array[x] = array[i-1];
-		array[i-1] = swp;
-		x++;
+		bit = al >> i & 1;
+		if(bit == 0)
+			kill(id_pd, SIGUSR1);
+		else
+			kill(id_pd, SIGUSR2);
+		usleep(20);
+		//printf("%d",bit);
 		i--;
 	}
 	
-}
-char	*decmal_to_bin(int ab)
-{
-	int y;
-	char *array;
-	int mod = 0;
-
-	y = 0;
-	array = malloc(8);
-	if(!array)
-		return NULL;
-	while (ab > 0)
-	{
-		array[y] = 48 + (ab % 2);
-		ab /= 2;
-		y++;
-	}
-	array[y] = '\0';
-	rev_array(array, y);
-	return array;
-}
-void	ft_sent(int car,pid_t id_pd)
-{
-	char *array;
-	int x;
-	
-	x = 0;
-	array = decmal_to_bin(car);
-	while (array[x])
-	{
-		if(array[x] == '1')
-			kill(id_pd, SIGUSR2);
-		else
-			kill(id_pd, SIGUSR1);
-		//break;
-		printf("%c\n",array[x]);
-		usleep(100);
-		x++;
-	}
-	free(array);
-	
-}
-void dunc_sh(int al)
-{
-	int i=7;
-
-	while (i>=0)
-	{
-		int bit = al >> i & 1;
-		
-	printf("%d\n",bit);
-	i--;
-	}
 	
 }
 int main(int argc, char const *argv[])
 {
-	// pid_t id_pd;
-	// int i;
+	pid_t id_pd;
+	int i;
 
-	// id_pd = atoi(argv[1]);
-	// // printf("%d",id_pd);
-	// if (argc != 3)
-	// 	return 0;
-	// while (argv[2][i])
-	// {
-	// 	ft_sent(argv[2][i], id_pd);
-	// 	i++;
-	// }
-// bit =0;
- int bit = (1 << 6) ; //01000001 | 00000001  | 01000000÷
-
-	
-	//ft_sent(253,1);
-	dunc_sh('A');
-	printf("%d",bit);
+	id_pd = atoi(argv[1]);
+	if (argc != 3)
+		return 0;
+	while (argv[2][i])
+	{
+		decmal_to_bin(argv[2][i], id_pd);
+		//printf("\n");
+		i++;
+	}
+	decmal_to_bin(10, id_pd);
+	// printf("%c",10);
 	return 0;
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: hchadili <hchadili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:29:49 by hchadili          #+#    #+#             */
-/*   Updated: 2024/03/01 17:46:05 by hchadili         ###   ########.fr       */
+/*   Updated: 2024/03/01 23:06:07 by hchadili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+void put_str(char c)
+{
+    write(1,&c,1);
+}
 
-int a;
+int a[2];
 
-void sigusr1_handler(int sig){
+void sigusr_handler(int sig)
+{
 
-    if(sin)
+    int nums[8] = {128,64,32,16,8,4,2,1};
+
+    if(sig == SIGUSR1)
+    {
+        //printf("0\n");
+    }
+    else
+    {
+        //printf("1\n");
+        a[1] += nums[a[0]%8];
+    }
+    a[0]++;
+    if(a[0] % 8 == 0)
+    {
+        put_str(a[1]);
+        a[1] = 0; 
+    }
 }
 int main() {
     // Register signal handlers
-    signal(SIGUSR1, sigusr1_handler);
-    signal(SIGUSR2, sigusr2_handler);
+    int num;
+    signal(SIGUSR1, sigusr_handler);
+    signal(SIGUSR2, sigusr_handler);
 
     printf("PID: %d\n", getpid());
     printf("Waiting for signals...\n");
 
     while(1) {
         sleep(1);
-        printf("%d\n",a);
+        //printf("%d\n",a);
     }
 
     return 0;
